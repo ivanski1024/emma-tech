3c7d15e933da7f6ab9cfa46ede45f83aed4f56d8
Ivan Petrov <ivan.antonov.p@gmail.com>
2018-11-13 12:32:16 +0200

    truelayer authorization handled

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-const {AuthAPIClient, DataAPIClient} = require("truelayer-client");
-const app = require("express")();
+const { AuthAPIClient, DataAPIClient } = require("truelayer-client");
+import Express from 'express';
+import crypto from 'crypto';
 
+const app = Express();
 const redirect_uri = "http://localhost:5000/truelayer-redirect";
 
-// Create TrueLayer client instance
 const client = new AuthAPIClient({
-    client_id: "INSERT YOUR client_id HERE",
-    client_secret: "INSERT YOUR client_secret HERE"
+    client_id: "emmatech-owm9",
+    client_secret: "210aavljqh9em9rh77g49v"
 });
 
-// Define array of permission scopes
 const scopes = ["info", "accounts", "balance", "transactions", "offline_access", "cards"]
 
-// Construct url and redirect to the auth dialog
+
+
 app.get("/", (req, res) => {
-    const authURL = client.getAuthUrl(redirect_uri, scopes, "foobar");
+    let nonce = crypto.randomBytes(12);
+    const authURL = client.getAuthUrl(redirect_uri, scopes, nonce, null, null, true);
+    console.log(authURL)
     res.redirect(authURL);
 });
 
@@ -29,55 +32,3 @@ app.get("/truelayer-redirect", async (req, res) => {
 });
 
 app.listen(5000, () => console.log("Example app listening on port 5000..."));
-
-
-// import express from 'express'
-// import User from './User';
-// import { AuthAPIClient, DataAPIClient } from "truelayer-client";
-
-// let app = express();
-
-// const redirect_uri = "http://localhost:5000/truelayer-redirect";
-
-
-// let Router = express.Router();
-// let user = new User('testUser');
-// user.ping();
-
-// let Client = new AuthAPIClient({
-//     client_id: ''
-// })
-
-// console.log('Done!');
-
-
-
-
-
-
-// // Create TrueLayer client instance
-// const client = new AuthAPIClient({
-//     client_id: "INSERT YOUR client_id HERE",
-//     client_secret: "INSERT YOUR client_secret HERE"
-// });
-
-// // Define array of permission scopes
-// const scopes = ["info", "accounts", "balance", "transactions", "offline_access", "cards"]
-
-// // Construct url and redirect to the auth dialog
-// app.get("/", (req, res) => {
-//     const authURL = client.getAuthUrl(redirect_uri, scopes, "foobar");
-//     res.redirect(authURL);
-// });
-
-// // Retrieve 'code' query-string param, exchange it for access token and hit data api
-// app.get("/truelayer-redirect", async (req, res) => {
-//     const code = req.query.code;
-//     const tokens = await client.exchangeCodeForToken(redirect_uri, code);
-//     const info = await DataAPIClient.getInfo(tokens.access_token);
-
-//     res.set("Content-Type", "text/plain");
-//     res.send(`Access Token: ${JSON.stringify(info, null, 2)}`);
-// });
-
-// app.listen(5000, () => console.log("Example app listening on port 5000..."));
