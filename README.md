796392e1b0f20e0060472b1ae20c96b137a9a700
Ivan Petrov <ivan.antonov.p@gmail.com>
2018-11-14 11:34:27 +0200

    config files added. database connection created

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # emma-tech
 Test project for Emma Technologies
+
+Requirements:
+
+Python 3.7+
+mySQL 8.0.13
+
+
+
+to configure create file config/local.json containing this:
+{
+    "dbConfig": {
+        "user": "<mysql username>",
+        "password": "<mysql password>"
+    }, 
+    "trueLayerConfig": {
+        "client": {
+            "client_id": "<trueLayer clientId>",
+            "client_secret": "<trueLayer secret>"
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,34 +1,35 @@
 const { AuthAPIClient, DataAPIClient } = require("truelayer-client");
 import Express from 'express';
 import crypto from 'crypto';
+import mysql from 'mysql';
+import config from 'config';
 
 const app = Express();
-const redirect_uri = "http://localhost:5000/truelayer-redirect";
 
-const client = new AuthAPIClient({
-    client_id: "emmatech-owm9",
-    client_secret: "210aavljqh9em9rh77g49v"
-});
+const dbConfig = config.get('dbConfig');
+const trueLayerConfig = config.get('trueLayerConfig');
 
-const scopes = ["info", "accounts", "balance", "transactions", "offline_access", "cards"]
-
-
+const client = new AuthAPIClient(trueLayerConfig.client);
+let db = mysql.createConnection(dbConfig);
 
 app.get("/", (req, res) => {
     let nonce = crypto.randomBytes(12);
-    const authURL = client.getAuthUrl(redirect_uri, scopes, nonce, null, null, true);
+    const authURL = client.getAuthUrl(trueLayerConfig.redirect_uri, trueLayerConfig.scopes, nonce, null, null, true);
     console.log(authURL)
     res.redirect(authURL);
 });
 
-// Retrieve 'code' query-string param, exchange it for access token and hit data api
 app.get("/truelayer-redirect", async (req, res) => {
     const code = req.query.code;
-    const tokens = await client.exchangeCodeForToken(redirect_uri, code);
+    const tokens = await client.exchangeCodeForToken(trueLayerConfig.redirect_uri, code);
     const info = await DataAPIClient.getInfo(tokens.access_token);
 
     res.set("Content-Type", "text/plain");
     res.send(`Access Token: ${JSON.stringify(info, null, 2)}`);
 });
+
+app.get("/transactions", (req, res) => {
+    let userId = req.param.userId;
+})
 
 app.listen(5000, () => console.log("Example app listening on port 5000..."));
